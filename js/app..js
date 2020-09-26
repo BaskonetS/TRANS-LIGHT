c90ff02002330c9b638c4280f41c7a7babf53392
BaskonetS <iga123mboy@gmail.com>
2020-09-26 03:46:48 +0300

    first commit

---
+++ js/app..js
@@ -0,0 +1,56 @@
+$ (function() {
+
+   let slider = $ ("#slider__project");
+ 
+    slider.slick({
+      
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+    arrows: true,
+    
+  });
+
+
+ 
+
+});
+
+  /* tabs */
+
+$ (document).ready(function() {
+
+  $ ('.tabs-triggers__item').click(function(e){
+
+   e.preventDefault();
+
+   $('.tabs-triggers__item').removeClass('tabs-triggers__item--active');
+   $('.tabs-content__item').removeClass('tabs-content__item--active');
+
+   $(this).addClass('tabs-triggers__item--active');
+   $($(this).attr('href')).addClass('tabs-content__item--active');
+
+  });
+   
+  $('.tabs-triggers__item:first').click();
+   
+  
+ 
+
+});
+
+  /* tabs */
+
+ 
+ 
+ 
+  
+ 
+
+
+  
+
+
+
+
---
+++ js/rent__slider.js
@@ -0,0 +1,17 @@
+/* rent__slider */
+ 
+$ (function() {
+
+  let rent__slider = $ ("#rent__slider2");
+
+  rent__slider.slick({
+     
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   autoplay: true,
+   autoplaySpeed: 10000,
+   arrows: true,
+  
+ });
+
+});
